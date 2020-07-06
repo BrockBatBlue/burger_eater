cceee2a636a75a0d7a3c731ea5ddb684b0232319
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-07-06 13:29:42 -0500

    initial with all starting files prepared

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_names) VALUES ('turkey burger');
+INSERT INTO burgers (burger_names) VALUES ('chicken licken burger');
+INSERT INTO burgers (burger_names) VALUES ('spicy hot burger');
