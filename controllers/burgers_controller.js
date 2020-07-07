6013abc101c5beaa3dac074f39e1e3e53aada44a
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-07-07 15:58:11 -0500

    successfully moved devoured column to vomit column, which will throw it back to devour. Yum!

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    devour: req.body.devoured
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
