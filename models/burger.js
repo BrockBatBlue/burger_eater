da313ac62cd8dc0a35a1d657803309b0d8a595dd
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-07-07 15:43:03 -0500

    managed to get seeds showing on index page

--- models/burger.js
+++ models/burger.js
@@ -2,24 +2,24 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: (cb)=> {
+    orm.all("burgers",(res)=> {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: (cols, vals, cb)=> {
+    orm.create("burgers", cols, vals, (res)=> {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  update: (objColVals, condition, cb)=> {
+    orm.update("burgers", objColVals, condition, (res)=> {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  delete: (condition, cb)=> {
+    orm.delete("burgers", condition, (res)=> {
       cb(res);
     });
   }
